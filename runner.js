5198b9f1d889845e2990553910c8c6615cbb2f85
Walker <walkerrm@gmail.com>
2020-11-15 10:55:39 -0500

    first commit

---
+++ runner.js
@@ -0,0 +1,13 @@
+class Runner {
+  constructor() {
+    this.files = [];
+  }
+
+  async collectFiles(targetPath) {
+    const files = await fs.promises.readdir(targetPath);
+
+    return files;
+  }
+}
+
+module.exports = Runner;
