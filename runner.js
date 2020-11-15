8ff80ef32329a70516b8558c7da80de03d55b050
Walker <walkerrm@gmail.com>
2020-11-15 12:39:51 -0500

    implemented runner testing framework

--- runner.js
+++ runner.js
@@ -1,13 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+
 class Runner {
-  constructor() {
-    this.files = [];
-  }
+	constructor() {
+		this.testFiles = [];
+	}
 
-  async collectFiles(targetPath) {
-    const files = await fs.promises.readdir(targetPath);
+	async runTests() {
+		for (let file of this.testFiles) {
+      const beforeEaches = [];
+      global.beforeEach = (fn) => {
+        beforeEaches.push(fn);
+      };
+      global.it = (desc, fn) => {
+        beforeEaches.forEach(func => func());
+        try {
+          fn();
+          console.log(`OK - ${desc}`);
+			  } catch (err) {
+          console.log(`X - ${desc}`);
+          console.log(err);
+        }
+      };
+      require(file.name);
+		};
+	}
 
-    return files;
-  }
+	async collectFiles(targetPath) {
+		const files = await fs.promises.readdir(targetPath);
+
+		for (let file of files) {
+			const filepath = path.join(targetPath, file);
+			const stats = await fs.promises.lstat(filepath);
+
+			if (stats.isFile() && file.includes('.test.js')) {
+				this.testFiles.push({ name: filepath });
+			} else if (stats.isDirectory()) {
+				const childFiles = await fs.promises.readdir(filepath);
+
+				files.push(...childFiles.map((f) => path.join(file, f)));
+			}
+		}
+	}
 }
 
 module.exports = Runner;
