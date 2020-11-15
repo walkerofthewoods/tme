01bab770be024e25bf94964e3f8cd7977a1456a2
Walker <walkerrm@gmail.com>
2020-11-15 15:54:47 -0500

    implemented testing interface for Node and Web

--- runner.js
+++ runner.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
+const render = require('./render');
+
+const forbiddenDirs = ['node_modules']
 
 class Runner {
 	constructor() {
@@ -8,21 +12,29 @@ class Runner {
 
 	async runTests() {
 		for (let file of this.testFiles) {
-      const beforeEaches = [];
-      global.beforeEach = (fn) => {
+			console.log(chalk.grey(`---- ${file.shortName}`))
+			const beforeEaches = [];
+			global.render = render;
+			global.beforeEach = (fn) => {
         beforeEaches.push(fn);
       };
-      global.it = (desc, fn) => {
+      global.it = async (desc, fn) => {
         beforeEaches.forEach(func => func());
         try {
-          fn();
-          console.log(`OK - ${desc}`);
+          await fn();
+          console.log(chalk.green(`\tOK - ${desc}`));
 			  } catch (err) {
-          console.log(`X - ${desc}`);
-          console.log(err);
+					const message = err.message.replace(/\n/g, '\n\t\t');
+          console.log(chalk.red(`\tX - ${desc}`));
+          console.log(chalk.red('\t', err.message));
         }
-      };
-      require(file.name);
+			};
+			
+			try {
+				require(file.name);
+			} catch (err) {
+				console.log(chalk.red('\tX - Error Loading File', file.name));
+			}
 		};
 	}
 
@@ -34,8 +46,8 @@ class Runner {
 			const stats = await fs.promises.lstat(filepath);
 
 			if (stats.isFile() && file.includes('.test.js')) {
-				this.testFiles.push({ name: filepath });
-			} else if (stats.isDirectory()) {
+				this.testFiles.push({ name: filepath, shortName: file });
+			} else if (stats.isDirectory() && !forbiddenDirs.includes(file)) {
 				const childFiles = await fs.promises.readdir(filepath);
 
 				files.push(...childFiles.map((f) => path.join(file, f)));
